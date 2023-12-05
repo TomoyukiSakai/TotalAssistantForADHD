7c6589e6d93b12ca596d339b9e800b197260f161
tomoyuki <service_mogu@icloud.com>
2023-12-05 23:19:56 +0900

    Initial Commit

---
+++ TotalAssistant/TotalAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalAssistantApp.swift
+//  TotalAssistant
+//
+//  Created by Tomoyuki SAKAI on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct TotalAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
