bb69a981a2a69f3c04238fdea9f6d0cc69f5dffa
TomoyukiSakai <service_mogu@icloud.com>
2023-12-19 23:35:40 +0900

    メニューバー作成
    機能はこれから

--- TotalAssistant/ContentView.swift
+++ TotalAssistant/ContentView.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Tomoyuki SAKAI on 2023/12/05.
 //
+//Infinity指定でサイズが等分になったり、自動で必要なスペースをとってくれる
+
+
 
 import SwiftUI
 
@@ -12,97 +15,140 @@ struct ContentView: View {
     @ObservedObject var timerClass = TimerClass()
     
     //＠Stateをつけているので構造体の中でも数値を変換できる
-    @State var isDisabled = false
-    @State var isStopped = false
+    @State var isMoving = false
     
     var body: some View {
-        VStack {
-            
-            let minutesStr = String(timerClass.nowMinutes)
-            let secondsStr =
-                String(timerClass.nowSeconds)
-            Text("ポモドーロタイマー")
-                .font(.largeTitle)
-                /*位置変えない*/
-                .padding()
-                .foregroundColor(.white)
-                
-            Text("<解説>")
-                .foregroundColor(.white)
-            Text("20分の集中と5分の休憩で、\n集中力を維持します")
-                
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-            
-            Text(minutesStr + ":" + secondsStr)
-                /*なぜかframeを冒頭にしないといけない*/
-                .frame(maxWidth: .infinity)
-                .font(.largeTitle)
-                .padding()
-                .foregroundColor(.black)
-                .background(Color.white)
-             
-                .padding()
-            
-            Text("現在のセット数　\(timerClass.nowSet)")
-                
-                .foregroundColor(.white)
-            Image("tomato")
-                .resizable()
-                .frame(width:200,height:200)
-                .padding()
-            
-            HStack {
-                Button(action:{
-                    isDisabled = true
-                    timerClass.ProcessTimer(stopped: false)
-                    isStopped = false
-                }){
-                    Text("スタート")
-                   
-                }
-                .disabled(isDisabled)
-                .padding()
-                .background(Color.white)
-                .padding()
-                .foregroundColor(.black)
-                Button(action:{
-                    isStopped = true
-                    timerClass.StopTimer(stoppedTimer:true)
-                    isDisabled = false
-                }){
-                    Text("一時停止")
-                }
-                .disabled(isStopped)
-                .padding()
-                .background(Color.white)
-                .padding()
-                .foregroundColor(.black)
-                
-            }
-            
-            /*
+        VStack(spacing:0){
             HStack{
-                Button(action:{
-                    
-                }){
-                    Text("TOP")
-                }
-                Button(action:{
-                    
-                }){
-                    Text("タイマー")
-                }
+                Text("ポモドーロタイマー")
+                    .font(.title)
+                
             }
-             */
-        }
-        .frame(maxWidth: .infinity)
-        .frame(maxHeight: .infinity)
-        .background(Color.black)
+            .frame(maxWidth: .infinity)
+            .frame(height: 100)
+            .background(Color.green)
+            
+            
+            VStack{
+                Text("<解説>\nポモドーロタイマーは２５分の集中タイムと５分の休憩のセットからなる集中力UPのメソッドです。")
+                    .frame(maxWidth:300)
+                let minutesStr = String(timerClass.nowMinutes)
+                let secondsStr =
+                String(timerClass.nowSeconds)
+                
+                /*
+                 .frame(maxWidth: .infinity)
+                 .background(Color.red)
+                 
+                 
+                 
+                 /*位置変えない*/
+                 .background(Color.white)
+                 .padding()
+                 .foregroundColor(.black)
+                 */
+                
+                
+                
+                Text(minutesStr + ":" + secondsStr)
+                /*なぜかframeを冒頭にしないといけない*/
+                    .frame(maxWidth: .infinity)
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundColor(.black)
+                    .background(Color.white)
+                    .padding()
+                    
+                
+                Text("現在のセット数　\(timerClass.nowSet)")
+                
+                    .foregroundColor(.black)
+                Image("tomato")
+                    .resizable()
+                    .frame(width:200,height:200)
+                    .padding()
+                
+                HStack {
+                    
+                    Button(action:{
+                        timerClass.ProcessTimer(stopped: false)
+                        isMoving = true
+                    }){
+                        Text("スタート")
+                        
+                    }
+                    .disabled(isMoving)
+                    .padding()
+                    .background(isMoving ? Color.gray : Color.pink)
+                    .padding()
+                    .foregroundColor(isMoving ? Color.black : Color.white)
+                    
+                    Button(action:{
+                        timerClass.StopTimer(stoppedTimer:true)
+                        isMoving = false
+                    }){
+                        Text("一時停止")
+                    }
+                    .disabled(!isMoving)
+                    .padding()
+                    .background(isMoving ? Color.blue : Color.gray)
+                    .padding()
+                    .foregroundColor(.white)
+                    
+                }
+            
+                
+                
+            }
+            .frame(maxWidth: .infinity)
+            .frame(maxHeight: .infinity)
+            .background(Color.yellow)
+            
+            
+            HStack{
+                
+                Button(action:{
+                    
+                }){
+                    Text("トップ")
+                        .padding()
+                        .frame(maxWidth:.infinity)
+                        .frame(minHeight:80)
+                }
+                Button(action:{
+                    
+                }){
+                    Text("集中系")
+                        .padding()
+                        .frame(maxWidth:.infinity)
+                }
+                Button(action:{
+                    
+                }){
+                    Text("計画系")
+                        .padding()
+                        .frame(maxWidth:.infinity)
+                }
+                Button(action:{
+                    
+                }){
+                    Text("設定")
+                        .padding()
+                        .frame(maxWidth:.infinity)
+                }
+               
+                
+            }
+            .background(.black)
+            .foregroundColor(.white)
+            .font(.footnote)
+            
+            
+        }.edgesIgnoringSafeArea(.bottom)
         
-    
     }
 }
+
 
 #Preview {
     ContentView()
